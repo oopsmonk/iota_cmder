bc41bc3e58f1dfc38468dc5df834ab4bc0951d31
Sam Chen <sam.chen@iota.org>
2021-07-28 17:16:21 +0800

    clean up

--- cli_cmd.c
+++ cli_cmd.c
@@ -243,80 +243,43 @@ static struct {
   struct arg_int *port;
   struct arg_int *is_https;
   struct arg_end *end;
-} node_info_set_args;
+} node_set_args;
 
-static cli_err_t fn_node_info_set(int argc, char **argv) {
+static cli_err_t fn_node_set(int argc, char **argv) {
   printf("TODO\n");
   return CLI_OK;
 }
 
-static void cmd_register_node_info_set() {
-  node_info_set_args.url = arg_str1(NULL, NULL, "<url>", "IP or URI");
-  node_info_set_args.port = arg_int1(NULL, NULL, "<port>", "port number");
-  node_info_set_args.is_https = arg_int1(NULL, NULL, "<is_https>", "0 or 1");
-  node_info_set_args.end = arg_end(5);
-  // node_info_set_args.url->hdr.resetfn = (arg_resetfn*)arg_str_reset;
+static void cmd_register_node_set() {
+  node_set_args.url = arg_str1(NULL, NULL, "<host>", "hostname");
+  node_set_args.port = arg_int1(NULL, NULL, "<port>", "port number");
+  node_set_args.is_https = arg_int1(NULL, NULL, "<is_https>", "0 or 1");
+  node_set_args.end = arg_end(5);
 
   cli_cmd_t cmd = {
-      .command = "node_info_set",
-      .help = "Sets node info",
+      .command = "node_set",
+      .help = "Set connected node",
       .hint = " <url> <port> <is_https (0|1)> ",
-      .func = &fn_node_info_set,
-      .argtable = &node_info_set_args,
+      .func = &fn_node_set,
+      .argtable = &node_set_args,
   };
 
   utarray_push_back(cli_ctx.cmd_array, &cmd);
 }
 
 //==========CLIENT_CONF==========
-static cli_err_t fn_client_conf(int argc, char **argv) {
+static cli_err_t fn_node_conf(int argc, char **argv) {
   printf("TODO\n");
   return CLI_OK;
 }
 
-static void cmd_register_client_conf() {
+static void cmd_register_node_conf() {
   cli_cmd_t cmd = {
-      .command = "client_conf",
-      .help = "Show client configurations",
+      .command = "node_conf",
+      .help = "Show connected node configuration",
       .hint = NULL,
-      .func = &fn_client_conf,
+      .func = &fn_node_conf,
       .argtable = NULL,
-  };
-  utarray_push_back(cli_ctx.cmd_array, &cmd);
-}
-
-//==========CLIENT_CONF_SET==========
-static struct {
-  struct arg_int *mwm;
-  struct arg_int *depth;
-  struct arg_int *security;
-  struct arg_end *end;
-} client_conf_set_args;
-
-static cli_err_t fn_client_conf_set(int argc, char **argv) {
-  int nerrors = arg_parse(argc, argv, (void **)&client_conf_set_args);
-  if (nerrors != 0) {
-    arg_print_errors(stderr, client_conf_set_args.end, argv[0]);
-    return CLI_ERR_INVALID_ARG;
-  }
-
-  printf("TODO\n");
-  return CLI_OK;
-}
-
-static void cmd_register_client_conf_set() {
-  client_conf_set_args.mwm = arg_int1(NULL, NULL, "<mwm>", "9 for testnet, 14 for mainnet");
-  client_conf_set_args.depth =
-      arg_int1(NULL, NULL, "<depth>", "The depth as which Random Walk starts, 3 is a typicall value used by wallets.");
-  client_conf_set_args.security =
-      arg_int1(NULL, NULL, "<security>", "The security level of addresses, can be 1, 2 or 3");
-  client_conf_set_args.end = arg_end(4);
-  cli_cmd_t cmd = {
-      .command = "client_conf_set",
-      .help = "Set client configurations",
-      .hint = " <mwm> <depth> <security>",
-      .func = &fn_client_conf_set,
-      .argtable = &client_conf_set_args,
   };
   utarray_push_back(cli_ctx.cmd_array, &cmd);
 }
@@ -350,11 +313,11 @@ static cli_err_t fn_seed_set(int argc, char **argv) {
 }
 
 static void cmd_register_seed_set() {
-  seed_set_args.seed = arg_str1(NULL, NULL, "<seed>", "Hash with 81 trytes");
+  seed_set_args.seed = arg_str1(NULL, NULL, "<seed>", "A 64-character-string");
   seed_set_args.end = arg_end(2);
   cli_cmd_t cmd = {
       .command = "seed_set",
-      .help = "Set the SEED hash of this wallet",
+      .help = "Set the SEED of this wallet",
       .hint = " <seed>",
       .func = &fn_seed_set,
       .argtable = &seed_set_args,
@@ -362,133 +325,10 @@ static void cmd_register_seed_set() {
   utarray_push_back(cli_ctx.cmd_array, &cmd);
 }
 
-//==========ACCOUNT==========
-static cli_err_t fn_account_data(int argc, char **argv) {
-  printf("TODO\n");
-  return CLI_OK;
-}
-
-static void cmd_register_account_data() {
-  cli_cmd_t cmd = {
-      .command = "account",
-      .help = "Get account data",
-      .hint = NULL,
-      .func = &fn_account_data,
-      .argtable = NULL,
-  };
-  utarray_push_back(cli_ctx.cmd_array, &cmd);
-}
-
-//==========BALANCE==========
-static struct {
-  struct arg_str *address;
-  struct arg_end *end;
-} get_balance_args;
-
-static int fn_get_balance(int argc, char **argv) {
-  int ret_code = 0;
-  printf("TODO\n");
-  return ret_code;
-}
-
-static void cmd_register_get_balance() {
-  get_balance_args.address = arg_strn(NULL, NULL, "<address>", 1, 10, "Address hashes");
-  get_balance_args.end = arg_end(20);
-  cli_cmd_t get_balance_cmd = {
-      .command = "balance",
-      .help = "Get the balance from addresses",
-      .hint = " <addresses...>",
-      .func = &fn_get_balance,
-      .argtable = &get_balance_args,
-  };
-  utarray_push_back(cli_ctx.cmd_array, &get_balance_cmd);
-}
-
-//==========SEND==========
-static struct {
-  struct arg_str *receiver;
-  struct arg_str *value;
-  struct arg_str *tag;
-  struct arg_str *remainder;
-  struct arg_str *message;
-  struct arg_end *end;
-} send_args;
-
-static int fn_send(int argc, char **argv) {
-  int ret_code = 0;
-  printf("TODO\n");
-  return CLI_OK;
-}
-
-static void cmd_register_send() {
-  send_args.receiver = arg_str1(NULL, NULL, "<RECEIVER>", "A receiver address");
-  send_args.value = arg_str0("v", "value", "<VALUE>", "A token value");
-  send_args.remainder = arg_str0("r", "remainder", "<REMAINDER>", "A remainder address");
-  send_args.message = arg_str0("m", "message", "<MESSAGE>", "a message for this transaction");
-  send_args.tag = arg_str0("t", "tag", "<TAG>", "A tag for this transaction");
-  send_args.end = arg_end(20);
-
-  cli_cmd_t send_cmd = {
-      .command = "send",
-      .help = "send value or data to the Tangle.\n\tex: send [RECEIVER_ADDRESS] -v=100",
-      .hint = " <receiver> -v <value> -m <message> -t <tag>",
-      .func = &fn_send,
-      .argtable = &send_args,
-  };
-  utarray_push_back(cli_ctx.cmd_array, &send_cmd);
-}
-
-//==========GEN_HASH==========
-uint8_t const tryte_chars[27] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-                                 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '9'};
-
-static struct {
-  struct arg_int *len;
-  struct arg_end *end;
-} gen_hash_args;
-
-static cli_err_t fn_gen_hash(int argc, char **argv) {
-  int nerrors = arg_parse(argc, argv, (void **)&gen_hash_args);
-  if (nerrors != 0) {
-    arg_print_errors(stderr, gen_hash_args.end, argv[0]);
-    return CLI_ERR_INVALID_ARG;
-  }
-
-  int len = gen_hash_args.len->ival[0];
-  char *hash = (char *)malloc(sizeof(char) * (len + 1));
-  if (hash == NULL) {
-    printf("Out of Memory\n");
-    return CLI_ERR_OOM;
-  }
-
-  srand(time(0));
-  for (int i = 0; i < len; i++) {
-    memset(hash + i, tryte_chars[rand() % 27], 1);
-  }
-  hash[len] = '\0';
-
-  printf("Hash: %s\n", hash);
-  free(hash);
-  return CLI_OK;
-}
-
-static void cmd_register_gen_hash() {
-  gen_hash_args.len = arg_int1(NULL, NULL, "<lenght>", "a length for the hash");
-  gen_hash_args.end = arg_end(2);
-  cli_cmd_t cmd = {
-      .command = "gen_hash",
-      .help = "Genrating a hash with the given length.\n  `gen_hash 81` for a random SEED",
-      .hint = " <length>",
-      .func = &fn_gen_hash,
-      .argtable = &gen_hash_args,
-  };
-  utarray_push_back(cli_ctx.cmd_array, &cmd);
-}
-
 //==========GET_ADDRESSES==========
 static struct {
   struct arg_str *start_idx;
-  struct arg_str *end_idx;
+  struct arg_str *count;
   struct arg_end *end;
 } get_addresses_args;
 
@@ -499,12 +339,12 @@ static cli_err_t fn_get_addresses(int argc, char **argv) {
 
 static void cmd_register_get_addresses() {
   get_addresses_args.start_idx = arg_str1(NULL, NULL, "<start>", "start index");
-  get_addresses_args.end_idx = arg_str1(NULL, NULL, "<end>", "end index");
+  get_addresses_args.count = arg_str1(NULL, NULL, "<count>", "number of addresses");
   get_addresses_args.end = arg_end(2);
   cli_cmd_t cmd = {
-      .command = "get_addresses",
-      .help = "Gets addresses by index",
-      .hint = " <start> <end>",
+      .command = "address",
+      .help = "Gets address by index",
+      .hint = " <start> <count>",
       .func = &fn_get_addresses,
       .argtable = &get_addresses_args,
   };
@@ -529,16 +369,17 @@ cli_err_t cli_command_init() {
   // registing commands
   cmd_register_help();
   cmd_register_version();
+
+  // configuration
+  cmd_register_node_set();
+  cmd_register_node_conf();
+
+  // client APIs
   cmd_register_node_info();
-  cmd_register_node_info_set();
-  cmd_register_client_conf();
-  cmd_register_client_conf_set();
+
+  // wallet APIs
   cmd_register_seed();
   cmd_register_seed_set();
-  cmd_register_account_data();
-  cmd_register_get_balance();
-  cmd_register_send();
-  cmd_register_gen_hash();
   cmd_register_get_addresses();
 
   return cli_wallet_init();
